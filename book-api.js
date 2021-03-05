901d05516dbb89a1803fc1008c7c8fc9195a6f39
Aaron Valadez Ruiz <aaron.valadez00@icloud.com>
2021-03-04 20:16:58 -0600

    Building the API

---
+++ book-api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+//Where we will keep books
+let books = [];
+app.use(cors());
+
+//Configuring body parser middleware 
+app.use(bodyParser.urlencoded({ extend: false}));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    //We will be coding here
+    const book = req.body;
+
+    //Output the book to the console for debbugin
+    console.log(book);
+    res.send('Book is added to the database');
+
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
